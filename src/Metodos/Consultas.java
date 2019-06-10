2140ce600cb9771d830a4f43237a97aab6a60ee5
raguiarperez <raguiarperez@danielcastelao.org>
2019-06-10 12:06:06 +0200

    Métodos Consultas

---
+++ src/Metodos/Consultas.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Metodos;
+
+import static Metodos.Conectar.conectar;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rafag
+ */
+public class Consultas {
+      
+    //Método para buscar filas de la tabla rally,
+    //especificando cualquiera de sus campos
+    //Devuelve un ArrayList con todos los campos de las filas
+
+    /**
+     *
+     * @param campo
+     * @param valor
+     * @return
+     */
+    public static ArrayList<String> consultaRally(String campo, Object valor){
+        ArrayList<String> rally = new ArrayList<>();
+        String sql = "SELECT Dorsal,Piloto,Vehiculo,Categoria"
+        + " FROM rally WHERE " + campo + "=?";
+        try (Connection conn = conectar();
+            PreparedStatement pstmt  = conn.prepareStatement(sql)){
+            pstmt.setObject(1, valor);
+            ResultSet rs  = pstmt.executeQuery();
+            while (rs.next()) {
+                rally.add(rs.getInt("Dorsal") +  "," + 
+                                   rs.getString("Piloto") + "," +
+                                   rs.getString("Vehiculo") + "," +
+                                   rs.getInt("Categoria"));
+            }
+            return rally;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            return rally;
+        }
+    }
+    
+    //Método que devuelve el nombre de la categoria, especificando el id
+
+    /**
+     *
+     * @param id
+     * @return resultado
+     */
+    public static String obtenerNombreCategoria(int id){
+        String sql = "SELECT nombre FROM Categoria where id = ?;";
+        String resultado = "";
+        try (Connection conn = conectar();
+            PreparedStatement pstmt  = conn.prepareStatement(sql)){
+            pstmt.setInt(1,id);
+            ResultSet rs  = pstmt.executeQuery();
+            while (rs.next()) {
+                resultado = rs.getString("nombre");
+            }
+            return resultado;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            return resultado;
+        }
+    }
+    
+    public static int obtenerIdCategoria(String nombre){
+        String sql = "SELECT id FROM Categoria where nombre = ?;";
+        int resultado = 0;
+        try (Connection conn = conectar();
+            PreparedStatement pstmt  = conn.prepareStatement(sql)){
+            pstmt.setString(1,nombre);
+            ResultSet rs  = pstmt.executeQuery();
+            while (rs.next()) {
+                resultado = rs.getInt("id");
+            }
+            return resultado;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            return resultado;
+        }
+    }
+}
