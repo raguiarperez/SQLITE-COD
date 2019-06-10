79d5068f274cde459dc204dc2de3ae80c421f664
raguiarperez <raguiarperez@danielcastelao.org>
2019-06-10 10:29:59 +0200

    Métodos Devolver

---
+++ src/Metodos/Devolver.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Metodos;
+
+import static Metodos.Conectar.conectar;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author rafag
+ */
+public class Devolver {
+    
+    //Método que devuelve un String con todos los campos de una fila,
+    //especificando el id
+
+    /**
+     *
+     * @param id
+     * @return resultado
+     */
+    public static String devolverCompetidor(int Dorsal){
+        String sql = "SELECT Dorsal,Piloto,Vehiculo,Categoria"
+        + " FROM rally WHERE Dorsal=?";
+        String resultado = "";
+        try (Connection conn = conectar();
+            PreparedStatement pstmt  = conn.prepareStatement(sql)){
+            pstmt.setInt(1,Dorsal);
+            ResultSet rs  = pstmt.executeQuery();
+            while (rs.next()) {
+                resultado = (rs.getInt("Dorsal") +  "," + 
+                                   rs.getString("Piloto") + "," +
+                                   rs.getString("Vehiculo") + "," +
+                                   rs.getInt("Categoria"));
+            }
+            return resultado;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            return resultado;
+        }
+    }
+    
+    
+    
+    //Método que devuelve un String con todos los campos de una fila,
+    //especificando el id
+
+    /**
+     *
+     * @param id
+     * @return resultado
+     */
+    public static String devolverCategoria(int id){
+        String sql = "SELECT id,nombre"
+        + " FROM Categoria WHERE id=?";
+        String resultado = "";
+        try (Connection conn = conectar();
+            PreparedStatement pstmt  = conn.prepareStatement(sql)){
+            pstmt.setInt(1,id);
+            ResultSet rs  = pstmt.executeQuery();
+            while (rs.next()) {
+                resultado = (rs.getInt("id") +  "," + 
+                                   rs.getString("nombre"));
+            }
+            return resultado;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            return resultado;
+        }
+    }
+
+}
